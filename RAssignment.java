6f81dc2aab6b4b840b245f64f1f2db93f7a63f3c
Dnyaneshwariumap <dnyaneshwariumap994@gmail.com>
2022-11-05 14:32:55 +0530

    Added:User last name validation method

--- RAssignment.java
+++ RAssignment.java
@@ -3,9 +3,11 @@ package Rejex;
 public class RAssignment {
     public static void main(String[]args)
     {
+
         System.out.println("------------------- Pattern Matching Assignment -------------------");
         ValidateUserInfo validateUserInfo = new ValidateUserInfo();
         validateUserInfo.checkFirstName();// UC1
+        validateUserInfo.checkLastName();// UC2
     }
 
-}
+    }
