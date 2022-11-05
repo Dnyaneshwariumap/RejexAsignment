f328f39a805d5f77b3a42be8a977938bbbfe5d2b
Dnyaneshwariumap <dnyaneshwariumap994@gmail.com>
2022-11-05 12:19:08 +0530

    welcome

---
+++ RAssignment.java
@@ -0,0 +1,11 @@
+package Rejex;
+
+public class RAssignment {
+    public static void main(String[]args)
+    {
+        System.out.println("------------------- Pattern Matching Assignment -------------------");
+        ValidateUserInfo validateUserInfo = new ValidateUserInfo();
+        validateUserInfo.checkFirstName();// UC1
+    }
+
+}
